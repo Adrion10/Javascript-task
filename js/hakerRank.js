dcc89e151fa4f1aeaa6d1eca326a7ce899c01600
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-15 22:35:12 +0100

    const findDuplicates = (arr) => {

--- js/hakerRank.js
+++ js/hakerRank.js
@@ -17,3 +17,20 @@ const secondDecimal = 3.34;
 console.log(firstIntiger + Number(secondIntiger));
 console.log(firstDecimal + Number(secondDecimal));
 console.log(firstString + secondString);
+
+// Task 3 Find all duplictes in an Array?
+const findDuplicates = (arr) => {
+  let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
+  let results = [];
+  for (let i = 0; i < sorted_arr.length - 1; i++) {
+    if (sorted_arr[i + 1] == sorted_arr[i]) {
+      results.push(sorted_arr[i]);
+    }
+  }
+  return results;
+};
+
+let duplicatedArray = [9, 9, 111, 2, 3, 4, 4, 5, 7];
+console.log(
+  `The duplicates in ${duplicatedArray} are ${findDuplicates(duplicatedArray)}`
+);
